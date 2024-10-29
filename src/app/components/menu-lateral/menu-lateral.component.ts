9850f13fca204a216b05284e68eba70d71261f7a
Mike <mikesoares8@gmail.com>
2024-10-29 19:54:28 -0300

    Estilização menu lateral

--- src/app/components/menu-lateral/menu-lateral.component.ts
+++ src/app/components/menu-lateral/menu-lateral.component.ts
@@ -12,4 +12,26 @@ export class MenuLateralComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public alternaSubMenu(id : string): void  {
+    var elemento = document.getElementById(id);
+    var menu = elemento?.querySelector('[name="menu"]');
+    var submenus = elemento?.querySelector('[name="submenu"]');
+    var icone_seta = elemento?.querySelector('[name="icone_menu"]');
+
+    var acao = submenus?.classList.contains('sub-menu-on') ? 'desativar' : 'ativar';
+  
+    if(acao == "ativar"){
+      submenus?.classList.add('sub-menu-on');
+      menu?.classList.add('item-menu-colapse-on');
+      icone_seta?.classList.add('icone-menu-aberto');
+    }
+  
+    else{
+      submenus?.classList.remove('sub-menu-on');
+      menu?.classList.remove('item-menu-colapse-on');
+      icone_seta?.classList.remove('icone-menu-aberto');
+    }
+  }
 }
+
+

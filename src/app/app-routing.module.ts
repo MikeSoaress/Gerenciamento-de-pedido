e04a116fce7d829bf15ffd9c428e0f3256a1e69b
Mike <mikesoares8@gmail.com>
2024-10-30 19:01:50 -0300

    ocultando menu e rodape na pagina de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { FuncionariosComponent } from './pages/funcionarios/funcionarios.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'funcionarios', component: FuncionariosComponent },
 ];
 

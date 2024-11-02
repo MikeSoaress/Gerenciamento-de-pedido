7f535d23d5b7be104646ebaad2a94c98e173da80
Mike <mikesoares8@gmail.com>
2024-11-02 15:36:30 -0300

    Melhorias no código do menu

--- src/app/components/menu-lateral/menu-lateral.component.ts
+++ src/app/components/menu-lateral/menu-lateral.component.ts
@@ -12,52 +12,49 @@ export class MenuLateralComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public alternaSubMenu(id : string): void  {
-    var elemento = document.getElementById(id);
-    var menu = elemento?.querySelector('[name="menu"]');
-    var submenus = elemento?.querySelector('[name="submenu"]');
-    var icone_seta = elemento?.querySelector('[name="icone_menu"]');
+  public alternaSubMenu(id: string): void {
+    var container_menu = document.getElementById(id);
+    var elemento_menu = container_menu?.querySelector('[name="menu"]');
+    var elemento_submenu = container_menu?.querySelector('[name="submenu"]');
+    var icone_seta = container_menu?.querySelector('[name="icone_menu"]');
 
-    var acao = submenus?.classList.contains('sub-menu-on') ? 'desativar' : 'ativar';
-  
-    if(acao == "ativar"){
-      submenus?.classList.add('sub-menu-on');
-      menu?.classList.add('item-menu-colapse-on');
+    var menu_ativo: boolean = elemento_submenu?.classList.contains('sub-menu-on') ? true : false;
+
+    menu_ativo ? desabilitarSubMenu() : habilitarSubMenu();
+
+    function habilitarSubMenu() {
+      elemento_submenu?.classList.add('sub-menu-on');
+      elemento_menu?.classList.add('item-menu-colapse-on');
       icone_seta?.classList.add('icone-menu-aberto');
     }
-  
-    else{
-      submenus?.classList.remove('sub-menu-on');
-      menu?.classList.remove('item-menu-colapse-on');
+
+    function desabilitarSubMenu() {
+      elemento_submenu?.classList.remove('sub-menu-on');
+      elemento_menu?.classList.remove('item-menu-colapse-on');
       icone_seta?.classList.remove('icone-menu-aberto');
     }
   }
 
-  public alternaMenu(): void{
+  public alternaMenu(): void {
     var elementos_menu = document.getElementById('menu-ocultavel');
     var base_menu = document.getElementById('container-menu');
     var menu_ativo: boolean = elementos_menu?.classList.contains('ocultar') ? false : true;
 
-    if(menu_ativo)
-      desabilitarMenu();
-    
+    menu_ativo ? desabilitarMenu() : habilitarMenu();
 
-    else
-      habilitarMenu();
+    function habilitarMenu() {
+      base_menu?.classList.add('menu-on');
+      base_menu?.classList.remove('menu-off');
+      setTimeout(() => {
+        elementos_menu?.classList.remove('ocultar');
+      }, 50);
+    }
 
-      function habilitarMenu(){
-        base_menu?.classList.add('menu-on');
-        base_menu?.classList.remove('menu-off');
-        setTimeout(() => {
-          elementos_menu?.classList.remove('ocultar');
-        }, 50);
-      }
-
-      function desabilitarMenu(){
-        base_menu?.classList.remove('menu-on');
-        base_menu?.classList.add('menu-off');
-        elementos_menu?.classList.add('ocultar');
-      }
+    function desabilitarMenu() {
+      base_menu?.classList.remove('menu-on');
+      base_menu?.classList.add('menu-off');
+      elementos_menu?.classList.add('ocultar');
+    }
   }
 }
 
